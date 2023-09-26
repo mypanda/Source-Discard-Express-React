8664cce1e6177a960f8e86e293c9553a73f98233
tantan.li <tantan.li@hiscene.com>
2023-09-26 18:58:34 +0800

    添加friends reducer

--- discard-frontend/src/Dashboard/FriendsSideBar/PendingInvitationsList/PendingInvitationsList.js
+++ discard-frontend/src/Dashboard/FriendsSideBar/PendingInvitationsList/PendingInvitationsList.js
@@ -1,44 +1,36 @@
-import React from 'react'
-import { styled } from '@mui/system'
-import PendingInvitationsListItem from './PendingInvitationsListItem'
+import React from "react";
+import { styled } from "@mui/system";
+import PendingInvitationsListItem from "./PendingInvitationsListItem";
+import { connect } from "react-redux";
 
-const DUMMY_INVITATION = [
-  {
-    _id: '1',
-    senderId: {
-      username: 'Mark',
-      mail: 'dummy@ad.com'
-    }
-  },
-  {
-    _id: '2',
-    senderId: {
-      username: 'John',
-      mail: 'dummy@ad.com'
-    }
-  }
-]
-const MainContainer = styled('div')({
-  width: '100%',
-  height: '22%',
-  display: 'flex',
-  flexDirection: 'column',
-  alignItems: 'center',
-  overflow: 'auto'
-})
+const MainContainer = styled("div")({
+  width: "100%",
+  height: "22%",
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+  overflow: "auto",
+});
 
-const PendingInvitationsList = () => {
+const PendingInvitationsList = ({ pendingFriendsInvitations }) => {
   return (
     <MainContainer>
-      {DUMMY_INVITATION.map((invitaion) => (
-      <PendingInvitationsListItem
-        key={invitaion._id}
-        id={invitaion._id}
-        username={invitaion.senderId.username}
-        mail={invitaion.senderId.mail} />
-    ))}
+      {pendingFriendsInvitations.map((invitaion) => (
+        <PendingInvitationsListItem
+          key={invitaion._id}
+          id={invitaion._id}
+          username={invitaion.senderId.username}
+          mail={invitaion.senderId.mail}
+        />
+      ))}
     </MainContainer>
-  )
-}
+  );
+};
 
-export default PendingInvitationsList
+const mapStoreStateToProps = ({ friends }) => {
+  return {
+    ...friends,
+  };
+};
+
+export default connect(mapStoreStateToProps)(PendingInvitationsList);
--- readme.md
+++ readme.md
@@ -1,5 +1,5 @@
 https://www.bilibili.com/video/BV1Eh4y1k7rd
-2:06:00
+2:26:00
 
 Node 14.16.1
 Npm 6.14.12
